8068712ce95e13c30628841aa30252323d6f991e
Jz-wutao <592536422@qq.com>
2019-01-08 21:35:56 +0800

    2-home页面搭建完成

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,11 +1,135 @@
 <template>
+  <el-container class="home-container">
+  <el-header class="home-header">
+    <el-row type="flex" align="middle">
+      <el-col :span="6">
+        <img src="@/assets/images/logo.png" alt="">
+      </el-col>
+      <el-col>
+        <h1>电商后台管理系统</h1>
+      </el-col>
+      <el-col :span="6">
+        <div>
+          欢迎全球至尊VIP吴先生
+          <a href="javascript:;" class="logout" @click="logout">退出</a>
+        </div>
+      </el-col>
+    </el-row>
+  </el-header>
 
+  <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px" class="home-aside">
+        <el-menu
+          :default-active="$route.path.slice(1)"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :router="true"
+          :unique-opened="true"
+        >
+          <el-submenu :index=" level1.order + '' " v-for="level1 in menus" :key="level1.id">
+            <!-- 一级菜单的图标和名称： -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{ level1.authName }}</span>
+            </template>
+            <!-- 二级菜单： -->
+            <!-- 注意：此处没有添加 "/" 将来会有个bug -->
+            <el-menu-item :index="level2.path" v-for="level2 in level1.children" :key="level2.id">
+              <!-- 二级菜单的图标和名称： -->
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ level2.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主内容区 -->
+      <el-main>
+        <!-- 子路由出口： -->
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    return {
+      menus: []
+    }
+  },
+  created () {
+    console.log('路由对象：', this.$route)
+    this.getMenuList()
+  },
+  methods: {
+    async getMenuList () {
+      const res = await this.$http.get('/menus')
+      this.menus = res.data.data
+    },
+    // 退出功能
+    async logout () {
+      try {
+        await this.$confirm('您是否确定退出?', '温馨提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+
+        // 退出登录逻辑：
+        this.$router.push('/login')
+        localStorage.removeItem('token')
+      } catch (e) {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      }
+    }
+  }
+}
 </script>
 
 <style>
+.home-container {
+  height: 100%;
+  background-color: #eaeef1;
+}
 
+.home-header {
+  background-color: #b3c1cd;
+}
+
+.home-header img {
+  width: 200px;
+}
+
+.home-header h1 {
+  margin: 0;
+  text-align: center;
+  color: #fff;
+  font-size: 28px;
+  font-weight: bolder;
+}
+
+.home-header div {
+  min-width: 235px;
+  font-weight: bold;
+}
+
+.home-header .logout {
+  color: #daa520;
+}
+
+.home-aside {
+  background-color: #545c64;
+}
+
+.el-header {
+  padding-left: 0;
+}
 </style>
